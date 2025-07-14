2e2ca15243ff0bc1a9cee582e01ba0694d56018e
榆柳松 <wzhengsen@gmail.com>
2025-07-14 14:18:01 +0800

    删除了一些不必要的测试代码。

--- test/main.cpp
+++ test/main.cpp
@@ -42,40 +42,14 @@ static void WriteFileRNG(std::string_view file) {
 }
 
 int main() {
-    std::cout << "-- Start benchmarking --" << std::endl;
-
-    constexpr uint64_t TryTimes = 100'000'000;
-    auto RandomGenerator        = Sodium::RandomGenerator();
-    const auto minRange         = Rand(RandomGenerator, 1u, 50u);
-    const auto maxRange         = Rand(RandomGenerator, minRange + 10u, minRange + 20u);
-
-    auto times = std::vector<uint64_t>(maxRange - minRange + 1u);
-    for (uint64_t i = 0; i < TryTimes; ++i) {
-        const auto rand = Rand(RandomGenerator, minRange, maxRange);
-        assert(rand >= minRange && rand <= maxRange);
-
-        const auto idx = rand - minRange;
-        ++times[idx];
-    }
-
-    std::cout << "minRange = " << minRange << std::endl;
-    std::cout << "maxRange = " << maxRange << std::endl;
-    std::cout << "Standard expectation = " << std::fixed << std::setprecision(4) << 1. / (maxRange - minRange + 1) * TryTimes << std::endl;
-    std::cout << "Standard probability = " << 1. / (maxRange - minRange + 1) * 100. << "%" << std::endl;
-    std::cout << std::endl;
-
-    for (size_t i = 0; i < times.size(); ++i) {
-        const auto probability = double(times[i]) / TryTimes * 100.;
-        std::cout << i + minRange << " -> " << times[i] << " times," << std::setprecision(4) << probability << "% probability." << std::endl;
-    }
-
-    std::cout << "-- End benchmarking --" << std::endl;
-
-    std::cout << "-- Start rng file --" << std::endl;
+    std::cout << "-- Write rng1.csv file --" << std::endl;
     WriteFileRNG("rng1.csv");
+
+    std::cout << "-- Write rng2.csv file --" << std::endl;
     WriteFileRNG("rng2.csv");
+
+    std::cout << "-- Write rng3.csv file --" << std::endl;
     WriteFileRNG("rng3.csv");
-    std::cout << "-- End rng file --" << std::endl;
 
     return 0;
 }
