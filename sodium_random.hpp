f0a367f13f04c50e5d8a8558a16a65accc546ee3
榆柳松 <wzhengsen@gmail.com>
2025-07-14 14:42:01 +0800

    一点点格式问题。

--- sodium_random.hpp
+++ sodium_random.hpp
@@ -4,23 +4,23 @@
 #include <cassert>
 
 namespace Sodium {
-    struct RandomGenerator {
-        using result_type = uint32_t;
+struct RandomGenerator {
+    using result_type = uint32_t;
 
-        static constexpr result_type min() { return 0; }
-        static constexpr result_type max() { return 0xFFFFFFFF; }
+    static constexpr result_type min() { return 0; }
+    static constexpr result_type max() { return 0xFFFFFFFF; }
 
-        result_type operator()() noexcept;
-    };
+    result_type operator()() noexcept;
+};
 
-    template <class T>
-    [[nodiscard]] T Rand(auto&& gen, T min, T max) {
-        assert(min <= max);
-        if constexpr (std::is_integral<T>::value) {
-            return std::uniform_int_distribution<T>(min, max)(gen);
-        }
-        else {
-            return std::uniform_real_distribution<T>(min, max)(gen);
-        }
+template <class T>
+[[nodiscard]] T Rand(auto&& gen, T min, T max) {
+    assert(min <= max);
+    if constexpr (std::is_integral<T>::value) {
+        return std::uniform_int_distribution<T>(min, max)(gen);
     }
+    else {
+        return std::uniform_real_distribution<T>(min, max)(gen);
     }
+}
+}  // namespace Sodium
